8c7edaabbee1087776a64f549dc4687fe42c6c1e
Hadrien Mary <hadim@users.noreply.github.com>
2017-04-21 18:58:28 -0400

    WIP

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -2,7 +2,7 @@ rem @echo on
 
 "%PREFIX%\Library\bin\java.exe" -classpath "%PREFIX%\opt\scijava-jupyter-kernel\*"^
                                 "org.scijava.jupyter.commands.InstallScijavaKernel" ^
-                                -pythonBinaryPath "%PREFIX%\Scripts\python.exe" ^
+                                -pythonBinaryPath "%PYTHON%" ^
                                 -verbose "info" ^
                                 -classpath "%PREFIX%\opt\scijava-jupyter-kernel\*" ^
                                 -javaBinaryPath "%PREFIX%\Library\bin\java.exe" ^
